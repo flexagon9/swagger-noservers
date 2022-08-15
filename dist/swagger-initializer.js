48725aafddd13e187c3e878f6bfa92a16ade53ee
karl.henselin@flexagon.com <karl.henselin@flexagon.com>
2022-08-15 12:59:50 -0500

    remove top bar and servers section update initializer to php

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,8 +3,9 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "openapi.php",
     dom_id: '#swagger-ui',
+    docExpansion: 'none',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
@@ -13,7 +14,8 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    tagsSorter: 'alpha'
   });
 
   //</editor-fold>
